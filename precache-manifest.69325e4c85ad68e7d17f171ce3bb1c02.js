e0a31ae9f01addb81eb28a89fa83b9255b0b7379
iheathers <shresthapritam7@gmail.com>
2020-07-22 12:57:10 +0545

    Updates

---
+++ precache-manifest.69325e4c85ad68e7d17f171ce3bb1c02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55037594d4416fa647772487fd8a0a75",
+    "url": "/songs-redux/index.html"
+  },
+  {
+    "revision": "571b1aa6306f89c926db",
+    "url": "/songs-redux/static/js/2.ebf5ddab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/songs-redux/static/js/2.ebf5ddab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd7dc7f740f25e07c49",
+    "url": "/songs-redux/static/js/main.b2515e1b.chunk.js"
+  },
+  {
+    "revision": "d15f211a16e41dd6ab14",
+    "url": "/songs-redux/static/js/runtime-main.a061e687.js"
+  }
+]);
